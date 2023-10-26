580dd7d2d643ee81ae9158d3bab8df614f9a4853
Szymon Szylhabel <szymon.szylhabel@gmail.com>
2023-10-26 13:42:37 +0200

    factory

---
+++ 3. DocFlow-Factory/DocFlow.Domain/Documents/Exporter/CSVExporter.cs
@@ -0,0 +1,34 @@
+﻿using DocFlow.Domain.Users;
+using System;
+using System.IO;
+
+namespace DocFlow.Domain.Documents
+{
+  public class CSVExporter : IExporter
+  {
+    private DocumentNumber _number;
+    private User _author;
+
+    public Stream Build()
+    {
+      MemoryStream s = new MemoryStream();
+      using (StreamWriter sw = new StreamWriter(s))
+      {
+        sw.Write(_number);
+        sw.Write(";");        
+      }
+
+      return s;
+    }
+
+    public void ExportAuthor(User author)
+    {
+      _author = author;
+    }
+
+    public void ExportNumber(DocumentNumber number)
+    {
+      _number = number;
+    }    
+  }
+}
---
+++ 3. DocFlow-Factory/DocFlow.Domain/Documents/Exporter/IExporter.cs
@@ -0,0 +1,12 @@
+﻿using DocFlow.Domain.Users;
+using System.IO;
+
+namespace DocFlow.Domain.Documents
+{
+  public interface IExporter
+  {    
+    void ExportNumber(DocumentNumber number);
+    void ExportAuthor(User author);
+    Stream Build();
+  }
+}
